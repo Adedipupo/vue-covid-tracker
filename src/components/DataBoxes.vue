132abd6afae38d213825442eaff113fd568d6b8f
Benedict <jokanolaadedipupo@gmail.com>
2021-08-24 02:45:06 +0100

    data boxes

--- src/components/DataBoxes.vue
+++ src/components/DataBoxes.vue
@@ -13,6 +13,20 @@
         {{ stats.TotalConfirmed }}
       </div>
     </div>
+
+     <div class="shadow-md bg-blue-100 p-10 text-center rounded">
+      <div class="h3 text-3xl text-blue-900 font-bold mb-4">
+        Deaths
+      </div>
+      <div class="text-2xl mb-4">
+        <span class="font-bold">New :</span>
+        {{ stats.NewDeaths }}
+      </div>
+      <div class="text-2xl mb-4">
+        <span class="font-bold">Total :</span>
+        {{ stats.TotalDeaths }}
+      </div>
+    </div>
   </div>
 </template>
 
