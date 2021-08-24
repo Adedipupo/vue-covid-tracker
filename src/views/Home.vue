785f833667b6109d0dd27251dabc3063ccb6a8c6
Benedict <jokanolaadedipupo@gmail.com>
2021-08-24 01:52:02 +0100

    fetching summary data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,6 +11,17 @@ export default {
   name: 'Home',
   components: {
     
+  },
+  methods:{
+    async fetchCovidData(){
+      const res = await fetch('https://api.covid19api.com/summary')
+      const data = await res.json()
+      return data;
+    }
+  },
+  async created(){
+    const data = await this.fetchCovidData();
+    console.log(data);
   }
 }
 </script>
